57146c5a62db3ef576e7baeba40c8a61da87716b
Stephen Saciolo <ssaciolo@gmail.com>
2019-06-20 21:14:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,9 +7,17 @@ var chocolateBars =
   ]
 
 function addElementToBeginningOfArray(arr, element){
-    
+    return
 }
   
 function destructivelyAddElementToBeginningOfArray(arr, element){
     return arr.unshift("element");
+}
+
+function addElementToEndOfArray(){
+  
+}
+
+function destructivelyAddElementToEndOfArray(){
+  
 }
