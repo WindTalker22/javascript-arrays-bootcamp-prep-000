b09ea10203259806d0943907695e10e8b5b2e43e
Stephen Saciolo <ssaciolo@gmail.com>
2019-06-20 21:08:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
+var chocolateBars = 
+[
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
 
-
+function addElementToBeginningOfArray(arr, element){
+    
+}
   
+function destructivelyAddElementToBeginningOfArray(arr, element){
+    return
+}
